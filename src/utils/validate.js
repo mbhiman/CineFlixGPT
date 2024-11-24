edb960ecb366be2daf3e29e257172628b13be2aa
Bhiman Singha <mbhiman70@gmail.com>
2024-11-24 21:35:25 +0530

    Fix unnecessary escape characters in email validation regex

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,7 +1,7 @@
 const checkValidData = (email, password) => {
-    const isEmailValid = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,6}$/.test(email);
+    const isEmailValid = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,6}$/.test(email);
     const isPasswordValid = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
-
+   
     if(!isEmailValid) {
         return "Email is not valid";
     }
