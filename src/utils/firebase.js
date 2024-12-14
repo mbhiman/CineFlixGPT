ee690ee06df526a8606868a3fcdc62e9191eea59
Bhiman Singha <mbhiman70@gmail.com>
2024-12-15 01:03:04 +0530

    Integrate Firebase for backend and deploy the project

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDIdLUF9PfxIabwfbeheFnmVOwZzI_kKB8",
+  authDomain: "cineflixgpt-6495a.firebaseapp.com",
+  projectId: "cineflixgpt-6495a",
+  storageBucket: "cineflixgpt-6495a.firebasestorage.app",
+  messagingSenderId: "13624842481",
+  appId: "1:13624842481:web:d3aa327677a0ab6cd2b150",
+  measurementId: "G-V594PXWHVC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
